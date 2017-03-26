52b6490202838720a5a932fa8adbd931a06198a3
vinod dauliya <vinod.kumar.itbhu.06@gmail.com>
2017-03-26 21:29:20 +0530

    -- added first cut of the project setup

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import '../../assets/css/style.css'
+
+@Component({
+    selector: 'lrn-ng2-app',
+    templateUrl: './app.component.html',
+})
+export class AppComponent{
+
+}
